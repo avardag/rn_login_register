25f9ccc06271aa8cace25882391f2d0b8184e383
avardag <steklofan@gmail.com>
2022-11-28 11:41:01 +0100

    fix auth flow

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,17 +1,43 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {Text} from 'react-native';
 import AuthNavigator from './AuthNavigator';
 import DrawerNavigator from './DrawerNavigator';
-import HomeNavigator from './HomeNavigator';
 import {GlobalContext} from '../context/Provider';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {ActivityIndicator} from 'react-native';
 
 export default function AppNavContainer() {
   const {authState} = useContext(GlobalContext);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [authCheckCompleted, setAuthCheckCompleted] = useState(false);
 
+  async function getUserFromStorage() {
+    try {
+      const user = await AsyncStorage.getItem('user');
+      if (user) {
+        setIsAuthenticated(true);
+        setAuthCheckCompleted(true);
+      } else {
+        setIsAuthenticated(false);
+        setAuthCheckCompleted(true);
+      }
+    } catch (error) {}
+  }
+
+  useEffect(() => {
+    getUserFromStorage();
+  }, []);
+
+  if (!authCheckCompleted) {
+    return <ActivityIndicator style={{flex: 1}} size="large" />;
+  }
   return (
     <NavigationContainer>
-      {authState.isLoggedIn ? <DrawerNavigator /> : <AuthNavigator />}
+      {authState.isLoggedIn || isAuthenticated ? (
+        <DrawerNavigator />
+      ) : (
+        <AuthNavigator />
+      )}
     </NavigationContainer>
   );
 }
